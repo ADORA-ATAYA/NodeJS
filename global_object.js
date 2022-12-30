542b6a8da465cb248615e4427cad471767078d94
ADORA-ATAYA <pragyanshutayal2100@gmail.com>
2022-12-30 18:00:14 +0530

    second day

---
+++ global_object.js
@@ -0,0 +1,9 @@
+// __dirname
+// ___filename
+// require
+// module
+// exports
+console.log(__filename)
+
+const data = require('./student');
+console.log(data);
---
+++ student.js
@@ -0,0 +1,10 @@
+console.log('pragyanshu');
+console.log('tanya');
+
+module.exports={
+age:20,
+name:'veshesh',
+user:function(){
+    console.log('user function')
+}
+}
